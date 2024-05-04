09389023c55cbd9c411a6767548bea530d8e9da8
aryan <aryanbasu005@gmail.com>
2024-05-04 12:27:44 +0530

    initial starting commit

---
+++ src/Components/Job.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+const Job = () => {
+    
+    useEffect(() => {
+        
+    }, [])
+    
+    return (
+        
+        <>
+        <h1>Weekday</h1>
+        </>
+    )
+}
+
+export default Job;
